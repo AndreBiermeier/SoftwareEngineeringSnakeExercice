39ba8b3d6f9e9ac63155b9435fd325c64663b431
AndreBiermeier <165042098+AndreBiermeier@users.noreply.github.com>
2024-12-10 12:37:36 +0100

    added class Obstacle which holds all the obstacle tiles and a method that places an obstacle on the board

---
+++ src/de/hsaalen/Obstacles.java
@@ -0,0 +1,46 @@
+package de.hsaalen;
+
+import java.util.ArrayList;
+
+public class Obstacles{
+    ArrayList<Coordinate> obstacles;
+    public Obstacles(){
+        this.obstacles = new ArrayList<Coordinate>();
+    }
+
+    public void generateIShapedObstacle(Coordinate start,Direction direction,ArrayList<Coordinate> snake_parts, Coordinate apple_coordinate, int board_width_in_tiles, int board_height_in_tiles){
+        boolean valid_location_found = false;
+        ArrayList<Coordinate> obstacle = new ArrayList<Coordinate>();
+        
+        for(int attempts_to_place_obstacle=0;attempts_to_place_obstacle<20 && !valid_location_found;attempts_to_place_obstacle++){
+            obstacle.add(Coordinate.giveRandomCoordinate(board_width_in_tiles,board_height_in_tiles));
+            if(direction == Direction.up || direction == Direction.down){
+                obstacle.add(new Coordinate(obstacle.get(0).x,obstacle.get(0).y-1));
+                obstacle.add(new Coordinate(obstacle.get(0).x,obstacle.get(0).y+1));
+            }
+            else{
+                obstacle.add(new Coordinate(obstacle.get(0).x-1,obstacle.get(0).y));
+                obstacle.add(new Coordinate(obstacle.get(0).x+1,obstacle.get(0).y));
+            }
+            valid_location_found = isObstacleValid(obstacle,snake_parts,apple_coordinate,board_width_in_tiles,board_height_in_tiles);
+        }
+    }
+
+    public boolean isObstacleValid(ArrayList<Coordinate> obstacle,ArrayList<Coordinate> snake_parts, Coordinate apple_coordinate, int board_width_in_tiles, int board_height_in_tiles){
+        for(Coordinate tile : obstacle){
+            if(!tile.isCoordinateInRectangle(new Coordinate(0,0),board_width_in_tiles,board_height_in_tiles)) return false;
+
+            if(tile.isCoordinateInRectangle(new Coordinate(snake_parts.get(0).x-4, snake_parts.get(0).y-4),9,9)) return false;
+            for(Coordinate snake_part : snake_parts){
+                 if(tile.isCoordinateInRectangle(new Coordinate(snake_part.x-1, snake_part.y-1),3,3)) return false;
+            }
+
+            if(tile.isCoordinateInRectangle(new Coordinate(apple_coordinate.x-1, apple_coordinate.y-1),3,3)) return false;
+
+            for(Coordinate placed_obstacle_tile : obstacles){
+                if(tile.isCoordinateInRectangle(new Coordinate(placed_obstacle_tile.x-1, placed_obstacle_tile.y-1),3,3)) return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/de/hsaalen/ObstaclesTest.java
@@ -0,0 +1,31 @@
+package de.hsaalen;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class ObstaclesTest{
+    @Test
+    public void testIsObstacleValid(){
+        Obstacles obstacles = new Obstacles();
+        obstacles.obstacles.add(new Coordinate(10,10));
+        ArrayList<Coordinate> new_obstacle = new ArrayList<Coordinate>();
+        new_obstacle.add(new Coordinate(11,10));
+        Snake snake = new Snake();
+        Coordinate apple_coordinate = new Coordinate(20,20);
+        assertFalse(obstacles.isObstacleValid(new_obstacle,snake.parts,apple_coordinate,30,30));
+        new_obstacle.get(0).x=12;
+        assertTrue(obstacles.isObstacleValid(new_obstacle,snake.parts,apple_coordinate,30,30));
+        new_obstacle.get(0).x=5;
+        assertTrue(obstacles.isObstacleValid(new_obstacle,snake.parts,apple_coordinate,30,30));
+        new_obstacle.get(0).y=9;
+        assertFalse(obstacles.isObstacleValid(new_obstacle,snake.parts,apple_coordinate,30,30));
+        new_obstacle.get(0).y=90;
+        assertFalse(obstacles.isObstacleValid(new_obstacle,snake.parts,apple_coordinate,30,30));
+        new_obstacle.get(0).y=21;
+        new_obstacle.get(0).x=21;
+        assertFalse(obstacles.isObstacleValid(new_obstacle,snake.parts,apple_coordinate,30,30));
+    }
+}
